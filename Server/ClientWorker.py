c0341871e39274313315790fca77b6babfc38655
Sammy El-Sherif <elsherifsammy@gmail.com>
2022-12-09 15:49:10 -0700

    small client worker changes

--- Server/ClientWorker.py
+++ Server/ClientWorker.py
@@ -1,2 +1,27 @@
+from threading import Thread
 
-class ClientWorker
+
+class ClientWorker(Thread):
+
+    def __init__(self):
+        self.__connection = connection
+        self.__server = server
+        self.__keepRunningClient = True
+
+    @property
+    def connection(self):
+        return self.__connection
+
+    @property
+    def server(self):
+        return self.__server
+
+    @property
+    def keepRunningClient(self):
+        return self.__keepRunningClient
+
+    def run(self):
+        pass
+        # while (self.__keepRunningClient){
+        #
+        # }
